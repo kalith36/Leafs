6bc2e11a61abec8de13527a671679f3d7433779d
kalith <kalith1444@gmail.com>
2021-10-31 16:38:46 +0530

    Commiting the leafBot Framework with no changes

---
+++ src/main/java/com/leafBot/pages/Training.java
@@ -0,0 +1,66 @@
+package com.leafBot.pages;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.commons.collections4.iterators.ReverseListIterator;
+
+import com.sun.org.apache.xpath.internal.axes.ReverseAxesWalker;
+
+import sun.security.util.Length;
+
+public class Training {
+
+public static void main(String[] args ) {
+	
+	/*String A = "suheal chase";
+	StringBuffer b=new StringBuffer(A);
+	System.out.println(b.reverse());*/
+	
+	/*String C = "Syed afzal"; 
+	
+	char[] D= C.toCharArray();
+			
+			System.out.println(C.charAt(2));
+	
+	for(int i=D.length-1;i>=0;i--) {
+		
+		System.out.print(D[i]);
+	}*/
+	/*String C = "Hello World";
+	
+	ArrayList<String> D = new ArrayList<>();
+	
+	D.add(C);
+	System.out.println();*/
+	
+	String N = "Suh@1999";
+
+	System.out.println(N.concat("syed"));
+	
+
+	
+//for(int i=0;i<N.length();i++) {
+	
+	//if() {
+	
+	//System.out.print(N);
+	
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+	
+}
